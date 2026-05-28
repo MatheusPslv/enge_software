c7d64e266f1a8c5315c69eddcdc784656098c51e
Paulo Floriano <paulohflorian5@gmail.com>
2026-05-28 20:24:19 -0300

    Update alunos.sql
    Voltei ao q era antes, era so pra testar erro kaskfaksfaksf

--- alunos.sql
+++ alunos.sql
@@ -4,7 +4,7 @@ CREATE TABLE aluno(
     RA INT NOT NULL,
     nome VARCHAR(60) NOT NULL,
     ender_alu VARCHAR(200) NOT NULL, 
-    telefone VARCHAR(100),
+    telefone VARCHAR(120),
     CONSTRAINT pk_Aluno PRIMARY KEY (RA)
  );
 
