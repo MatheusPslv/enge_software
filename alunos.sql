e8cc3e5d034f5d71c3d44ca90e22bce1960ab47b
Paulo Floriano <paulohflorian5@gmail.com>
2026-05-28 20:21:53 -0300

    Update alunos.sql

--- alunos.sql
+++ alunos.sql
@@ -4,7 +4,7 @@ CREATE TABLE aluno(
     RA INT NOT NULL,
     nome VARCHAR(60) NOT NULL,
     ender_alu VARCHAR(200) NOT NULL, 
-    telefone VARCHAR(120),
+    telefone VARCHAR(100),
     CONSTRAINT pk_Aluno PRIMARY KEY (RA)
  );
 
